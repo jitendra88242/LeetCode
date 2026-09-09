0fabeaa5fcce19f96cb69cbf55e917bd53d8bc05
Jitendra Choudhary <jbc8824238426@gmail.com>
2026-09-09 21:04:06 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.92%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum=n*(n+1)/2;
+        int Sum=0;
+        for(int i=0;i<n;i++){
+             Sum+=nums[i];
+        }
+
+        return expectedSum-Sum;
+        
+    }
+};
